bfc01a28d0ff076107c96f8483be77f713ba4884
Sayako Kuroda <kuroda.sayako@fujitsu.com>
2024-01-12 12:39:24 +0900

    RouterをHashRouterに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
 import Page from "./pages/Page";
 
 const App: React.FC = () => {
   return (
-    <Router basename="/todo">
+    <Router>
       <Menu>
         <MenuButton as={Button}>きりかえ</MenuButton>
         <MenuList>
-          <MenuItem as={Link} to="/page1">
+          <MenuItem as={Link} to="page1">
             ほいくえんにいくまえ
           </MenuItem>
-          <MenuItem as={Link} to="/page2">
+          <MenuItem as={Link} to="page2">
             でかけるとき
           </MenuItem>
-          <MenuItem as={Link} to="/page3">
+          <MenuItem as={Link} to="page3">
             ねるまでにやること
           </MenuItem>
         </MenuList>
